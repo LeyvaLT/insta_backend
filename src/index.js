815e0503afae37152e6322c62370431345aef5b6
Edgar Leyva <edgar_leyva@hotmail.es>
2018-10-04 17:19:19 -0500

    se agrego configuracion para deploy a heroku

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ const server = new GraphQLServer({
         ...req,
         db: new Prisma({
             typeDefs: 'src/generated/prisma.graphql',
-            endpoint: process.env.PRISMA_ENDPOINT,
+            endpoint: PRISMA_ENDPOINT,
             debug:true
         })
     }),
